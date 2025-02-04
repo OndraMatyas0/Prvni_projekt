3811de5fcb70cea8db4e1b0b4de2e94488c6d200
OndraMatyas0 <ondrej.matyas@volny.cz>
2025-02-04 15:05:30 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+print("hello world")
+print("hello world"), ("15")
+#vypsání slova hello world a 15
+cislo = 1
+#vytvoření proměnné cislo a udaní jeji hodnoty na 1
+print("hello world", cislo)
+#vypsani stringu a proměné 
+text = "ahoj"
+print(text, cislo)
+#dvě proměnné
+vstupnipromena= input("Zadejte číslo ")
+print(vstupnipromena)
