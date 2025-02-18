692590fb98dd327f61c9c7bffaf33bee79b65977
OndraMatyas0 <ondrej.matyas@volny.cz>
2025-02-18 15:03:34 +0100

    Add files via upload

---
+++ input.py
@@ -0,0 +1,5 @@
+sklenice1 = input("Zadejte název nápoje = ")
+sklenice2 = input("Zadejte název nápoje = ")
+sklenice3= ""
+print(f"sklenice 1 = {sklenice1}")
+print(f"sklenice 2 = {sklenice2}")
---
+++ sklenice.py
@@ -0,0 +1,46 @@
+sklenice1 = "mlíko"
+sklenice2 = "pivson"
+sklenice3 = "prázdno"
+print ("Ve sklenici 1 se nachází-",sklenice1)
+print ("Ve sklenici 2 se nachází-",sklenice2)
+print ("Ve sklenici 3 se nachází-",sklenice3)
+
+
+
+print ("------------------------------------")
+print("Přeleju sklenici 2 do sklenice 3")
+sklenice3 = sklenice2
+sklenice2 = "prázdno"
+print ("Ve sklenici 1 se nachází-",sklenice1)
+print ("Ve sklenici 2 se nachází-",sklenice3)
+print ("Ve sklenici 3 se nachází-",sklenice2)
+
+
+
+print ("------------------------------------")
+print ("Do sklenice 2 přeleju sklenici 1")
+sklenice2 = sklenice1
+sklenice1 = "prázdno"
+print ("Ve sklenici 1 se nachází-",sklenice2)
+print ("Ve sklenici 2 se nachází-",sklenice1)
+print ("Ve sklenici 3 se nachází-",sklenice3)
+
+
+
+print ("------------------------------------")
+print ("Do sklenice 1 přeleju sklenici 3")
+sklenice1 = sklenice3
+sklenice3 = "prázdno"
+print ("Ve sklenici 1 se nachází-",sklenice3)
+print ("Ve sklenici 2 se nachází-",sklenice2)
+print ("Ve sklenici 3 se nachází-",sklenice1)
+
+
+
+print ("------------------------------------")
+print ("Ve sklenici 1 se nachází-",sklenice1)
+print ("Ve sklenici 2 se nachází-",sklenice2)
+print ("Ve sklenici 3 se nachází-",sklenice3)
+
+
+
